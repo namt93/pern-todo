c04ddf8470ec815dd9af8c90195eeaa147185f22
namt93 <namt7115633@gmail.com>
2024-07-23 12:02:59 +0700

    Update hard cords

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,8 +21,9 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
-server.listen(5000, () => {
-    console.log('Server running on http://localhost:5000/');
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}/`);
     
 })
 
