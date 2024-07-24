71e10da2b544f17bdc2805ef66b66187b3985888
namt93 <namt7115633@gmail.com>
2024-07-24 13:59:21 +0700

    Register APIs using sequelize ORM

--- server/src/router/authentication.ts
+++ server/src/router/authentication.ts
@@ -1,10 +1,23 @@
 import express from 'express';
 
-import { login, register, verifyUser } from '../controllers/authentication';
+import { 
+    login, 
+    register, 
+    verifyUser 
+} from '../controllers/authentication';
+import { 
+    loginORM, 
+    registerORM, 
+    verifyUserORM
+} from '../controllers/orm/authenticationController';
 import { isAuthenticated } from '../middlewares';
 
 export default (router: express.Router) => {
     router.post('/auth/register', register);
     router.post('/auth/login', login);
     router.get('/auth/verify-user', isAuthenticated, verifyUser);
+
+    router.post('/v1.1/auth/register', registerORM);
+    router.post('/v1.1/auth/login', loginORM);
+    router.get('/v1.1/auth/verify-user', isAuthenticated, verifyUserORM);
 }
